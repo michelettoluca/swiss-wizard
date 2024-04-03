f2ef348652530a803411bf290f8705afe664a1ba
Micheletto Luca <mch.luca00@gmail.com>
2024-04-03 21:03:44 +0200

    Sign in screen

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,9 +1,12 @@
-import { Pressable, Text } from "react-native"
-import { useCallback, useEffect } from "react"
+import { Pressable, View } from "react-native"
+import { useEffect } from "react"
 import { useAuth, useOAuth } from "@clerk/clerk-expo"
 import { useWarmUpBrowser } from "../hooks"
 import * as AuthSession from "expo-auth-session"
 import { Redirect, SplashScreen } from "expo-router"
+import { Text } from "../components"
+import { Colors, Size } from "../styles"
+import { GoogleIcon } from "../icons/google"
 
 SplashScreen.preventAutoHideAsync()
 
@@ -38,8 +41,43 @@ export default function () {
     }
 
     return (
-        <Pressable onPress={() => authenticate()}>
-            <Text>Sign-in</Text>
-        </Pressable>
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                padding: Size.base,
+                gap: Size.xxl
+            }}
+        >
+            <View style={{ alignItems: "center", padding: Size.l }}>
+                <Text color={Colors.gray[800]} size="xl" weight="bold">
+                    Swiss Wizard
+                </Text>
+                <Text align="center">Grande sottotitolo, importantissimo e su più righe con punto.</Text>
+            </View>
+            <View style={{ alignSelf: "stretch", borderRadius: 1000, overflow: "hidden" }}>
+                <Pressable
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        gap: Size.xs,
+                        paddingVertical: 16,
+                        borderColor: Colors.gray[300],
+                        borderWidth: 1,
+                        borderRadius: 1000
+                    }}
+                    android_ripple={{ color: "rgba(0, 0, 0, 0.05)", radius: 1000, foreground: false }}
+                    onPress={() => authenticate()}
+                >
+                    <GoogleIcon width={24} height={24} />
+                    <Text size="s" color={Colors.gray[600]}>
+                        Sign in with Google
+                    </Text>
+                </Pressable>
+            </View>
+        </View>
     )
 }
