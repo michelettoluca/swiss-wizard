5ff49c50ac3328df240ec52e857a372ffc680354
Micheletto Luca <mch.luca00@gmail.com>
2024-04-09 23:07:36 +0200

    feat(mobile): join host screen

--- apps/mobile/src/app/(app)/join-host.tsx
+++ apps/mobile/src/app/(app)/join-host.tsx
@@ -1,5 +1,38 @@
-import { View } from "lucide-react-native"
+import { useState } from "react"
+import { TextInput, View } from "react-native"
+import { GRAY } from "../../styles/color"
+import { BASE, XL } from "../../styles/size"
 
 export default function () {
-    return <View style={{ height: 1000, width: 1000, backgroundColor: "red" }}></View>
+    const [code, setCode] = useState<string>("")
+
+    return (
+        <View style={{ flex: 1, padding: BASE, alignItems: "stretch", justifyContent: "center" }}>
+            {/* <Button>
+                <View
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center"
+                    }}
+                >
+                    <Plus stroke={WHITE} />
+                    <Text color={WHITE}>Add</Text>
+                </View>
+            </Button> */}
+            <TextInput
+                placeholder="CODE"
+                placeholderTextColor={GRAY[400]}
+                cursorColor={GRAY[600]}
+                maxLength={7}
+                style={{
+                    fontFamily: "Inter SemiBold",
+                    fontSize: XL,
+                    textAlign: "center"
+                }}
+                value={code}
+                onChangeText={setCode}
+            />
+        </View>
+    )
 }
