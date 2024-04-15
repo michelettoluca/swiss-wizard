8255c9486bc32263d03248898dbdd6d93bc6d37e
Micheletto Luca <mch.luca00@gmail.com>
2024-04-15 23:01:17 +0200

    feat(mobile): select

--- apps/mobile/src/app/(app)/(tabs)/join-host/host.tsx
+++ apps/mobile/src/app/(app)/(tabs)/join-host/host.tsx
@@ -1,29 +1,42 @@
 import { useState } from "react"
 import { View } from "react-native"
 import { Input } from "../../../../components/input"
+import { Select } from "../../../../components/select"
 import { Text } from "../../../../components/text"
 import { GRAY } from "../../../../styles/color"
-import { BASE } from "../../../../styles/size"
+import { BASE, XS } from "../../../../styles/size"
 
 export default function () {
-    const [form, setForm] = useState<Record<string, string>>({
-        name: "asd"
+    const [form, setForm] = useState<{ name: string; select: number | null }>({
+        name: "asd",
+        select: null
     })
 
-    function handleChange(key: string, value: string) {
+    function handleChange<T extends keyof typeof form>(key: T, value: (typeof form)[T]) {
         setForm((form) => ({
             ...form,
             [key]: value
         }))
     }
+
     return (
-        <View style={{ flex: 1, backgroundColor: GRAY[100], padding: BASE }}>
+        <View style={{ flex: 1, backgroundColor: GRAY[100], padding: BASE, gap: XS }}>
             <Text>{JSON.stringify(form, null, 3)}</Text>
             <Input
                 label="Name"
                 placeholder="Name"
                 value={form.name}
                 onChange={(value) => handleChange("name", value)}
+            />
+            <Select
+                label="Format"
+                value={form.select}
+                onChange={(v) => handleChange("select", v)}
+                options={[
+                    { label: "One", value: 1 },
+                    { label: "Two", value: 2 }
+                ]}
+                placeholder="Format"
             />
         </View>
     )
--- apps/mobile/src/components/select.tsx
+++ apps/mobile/src/components/select.tsx
@@ -1,12 +1,106 @@
-import { View } from "lucide-react-native"
-import { Pressable } from "react-native"
+import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet"
+import { useRef } from "react"
+import { Keyboard, Pressable, StyleSheet } from "react-native"
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated"
+import { GRAY, WHITE } from "../styles/color"
+import { M, XS, XXXL, XXXS } from "../styles/size"
+import { Text } from "./text"
 
-export function Select() {
+type SelectProps<T> = {
+    label: string
+    placeholder: string
+    value: T
+    onChange: (value: T) => void
+    options: Option<T>[]
+}
+
+type Option<T> = {
+    label: string
+    value: T
+}
+
+export function Select<T>({ label, placeholder, value, onChange, options }: SelectProps<T>) {
+    const bottomSheetModalRef = useRef<BottomSheetModal>(null)
+
+    const selected = options.find((option) => JSON.stringify(option.value) === JSON.stringify(value))
+    const selectedIndex = selected ? options.indexOf(selected) : null
+
+    function handlePresentModalPress() {
+        Keyboard.dismiss()
+        bottomSheetModalRef.current?.present()
+    }
+    function closeModal() {
+        bottomSheetModalRef.current?.close({ duration: 75 })
+    }
+
     return (
-        <Pressable
-            style={{ position: "absolute", top: 0, right: 0, bottom: 0, left: 0, backgroundColor: "red", zIndex: 10 }}
-        >
-            <View />
-        </Pressable>
+        <>
+            <Pressable
+                style={StyleSheet.flatten([
+                    {
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        backgroundColor: WHITE,
+                        borderRadius: XS,
+                        paddingHorizontal: M,
+                        height: XXXL,
+                        gap: XXXS
+                    }
+                ])}
+                onPress={handlePresentModalPress}
+            >
+                <Text size={XS} color={GRAY[600]}>
+                    {label}
+                </Text>
+                <Text>{selected?.label ?? placeholder}</Text>
+            </Pressable>
+            <BottomSheetModal
+                ref={bottomSheetModalRef}
+                backdropComponent={() => (
+                    <Animated.View
+                        entering={FadeIn.duration(75)}
+                        exiting={FadeOut.duration(75)}
+                        style={{
+                            ...StyleSheet.absoluteFillObject,
+                            backgroundColor: "rgba(0, 0, 0, 0.25)"
+                        }}
+                    >
+                        <Pressable style={{ flex: 1 }} onPress={closeModal} />
+                    </Animated.View>
+                )}
+                snapPoints={["50%"]}
+                containerStyle={{
+                    borderRadius: 0
+                }}
+                style={{
+                    borderRadius: 0
+                }}
+            >
+                <BottomSheetView
+                    style={{
+                        flex: 1
+                    }}
+                >
+                    {options.map((option, i) => (
+                        <Pressable
+                            key={JSON.stringify(option.value)}
+                            style={{
+                                height: XXXL,
+                                alignItems: "center",
+                                justifyContent: "center",
+                                backgroundColor: selectedIndex === i ? GRAY[100] : undefined
+                            }}
+                            onPress={() => {
+                                closeModal()
+                                onChange(option.value)
+                            }}
+                        >
+                            <Text>{option.label}</Text>
+                        </Pressable>
+                    ))}
+                </BottomSheetView>
+            </BottomSheetModal>
+        </>
     )
 }
