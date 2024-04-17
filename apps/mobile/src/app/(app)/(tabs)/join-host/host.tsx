0c932912d7077f1a63155756d0350982ff2e5b24
Micheletto Luca <mch.luca00@gmail.com>
2024-04-17 18:37:12 +0200

    chore(mobile): dayly commit hehe

--- apps/mobile/src/app/(app)/(tabs)/join-host/host.tsx
+++ apps/mobile/src/app/(app)/(tabs)/join-host/host.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
+import { Controller, useForm } from "react-hook-form"
 import { View } from "react-native"
+import { Button } from "../../../../components/button"
 import { Input } from "../../../../components/input"
 import { Select } from "../../../../components/select"
 import { Text } from "../../../../components/text"
@@ -7,56 +8,79 @@ import { GRAY } from "../../../../styles/color"
 import { BASE, XS } from "../../../../styles/size"
 
 export default function () {
-    const [form, setForm] = useState<{ name: string; timelimit: number | null; select: number | null }>({
-        name: "",
-        timelimit: null,
-        select: null
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+        getValues
+    } = useForm<{
+        firstName: string
+        lastName: string
+        timeLimit: number
+        numeroACaso: number
+    }>({
+        defaultValues: {
+            firstName: "",
+            lastName: ""
+        }
     })
-
-    function handleChange<T extends keyof typeof form>(key: T, value: (typeof form)[T]) {
-        setForm((form) => ({
-            ...form,
-            [key]: value
-        }))
-    }
 
     return (
         <View style={{ flex: 1, backgroundColor: GRAY[100], padding: BASE, gap: XS }}>
-            <Text>{JSON.stringify(form, null, 3)}</Text>
-            <Input
-                label="Name"
-                placeholder="Name"
-                value={form.name}
-                onChange={(value) => handleChange("name", value)}
+            <Text>{JSON.stringify(getValues(), null, 3)}</Text>
+            <Controller
+                control={control}
+                name="firstName"
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <Input label="Name" placeholder="Name" value={value} onChange={onChange} />
+                )}
             />
-            <Input
-                label="Time limit"
-                placeholder="55"
-                suffix="min"
-                value={`${form.timelimit ?? ""}`}
-                onChange={(value) => handleChange("timelimit", value ? Number(value) : null)}
-                keyboardType="decimal-pad"
+            <Controller
+                control={control}
+                name="timeLimit"
+                rules={{
+                    min: 3
+                }}
+                render={({ field: { onChange, value, disabled } }) => (
+                    <Input
+                        label="Time limit"
+                        placeholder="55"
+                        keyboardType="decimal-pad"
+                        suffix="min"
+                        value={`${value ?? ""}`}
+                        onChange={(v) => onChange(v?.length ? Number(v) : null)}
+                    />
+                )}
             />
-            <Select
-                label="Format"
-                value={form.select}
-                onChange={(v) => handleChange("select", v)}
-                options={[
-                    { label: "One", value: 1 },
-                    { label: "Two", value: 2 },
-                    { label: "Three", value: 3 },
-                    { label: "Four", value: 4 },
-                    { label: "Five", value: 5 },
-                    { label: "Six", value: 6 },
-                    { label: "Seven", value: 7 },
-                    { label: "Eight", value: 8 },
-                    { label: "Nine", value: 9 },
-                    { label: "Ten", value: 10 },
-                    { label: "Eleven", value: 11 },
-                    { label: "Twelve", value: 12 }
-                ]}
-                placeholder="Format"
+            <Controller
+                control={control}
+                name="numeroACaso"
+                render={({ field: { onChange, value } }) => (
+                    <Select
+                        label="Format"
+                        value={value}
+                        onChange={onChange}
+                        options={[
+                            { label: "One", value: 1 },
+                            { label: "Two", value: 2 },
+                            { label: "Three", value: 3 },
+                            { label: "Four", value: 4 },
+                            { label: "Five", value: 5 },
+                            { label: "Six", value: 6 },
+                            { label: "Seven", value: 7 },
+                            { label: "Eight", value: 8 },
+                            { label: "Nine", value: 9 },
+                            { label: "Ten", value: 10 },
+                            { label: "Eleven", value: 11 },
+                            { label: "Twelve", value: 12 }
+                        ]}
+                        placeholder="Format"
+                    />
+                )}
             />
+            <Button onPress={handleSubmit(console.log)}>
+                <Text>Submit</Text>
+            </Button>
         </View>
     )
 }
